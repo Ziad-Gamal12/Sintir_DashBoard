fdb573dcac2d1dfce9b3aea4e5d70a08af191904
ziad gamal <ziadgemy123@gmail.com>
2026-01-29 21:45:47 +0200

    removed constants file for git ignore

---
+++ lib/constant.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+/// UI Constants
+
+const Color KMainColor = Color(0xff4169E2);
+const Color KSecondaryColor = Color(0xffff8c1a);
+const String currentVersion = "1.0.0";
+const String supaBaseUrl = "https://lsblauupczaxktmrpsmv.supabase.co";
+const String supaAnonKey = "sb_publishable_-xw94XQsYFQ_tUc6tRX8gQ_AO5bk-jk";
